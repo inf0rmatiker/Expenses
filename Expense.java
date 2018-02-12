c8c06f04abe870accb9a316aed1ba8c353d29be4
inf0rmatiker <inf0rmatiker@users.noreply.github.com>
2018-02-11 20:14:23 -0700

    Update Expense.java
    Removed hard-coded names

--- Expense.java
+++ Expense.java
@@ -5,24 +5,26 @@ import java.text.ParseException;
 
 
 public class Expense {
-	/* 1 for Grace, 0 for Caleb */
+	/* 0 for Person 1, 1 for Person 2 */
 	public int person;
-	
 	public double expenseAmount;
-	
 	public String description;
-	
 	public boolean paid;
-	
 	public Date date;
-	
 	public SimpleDateFormat dateFormat;
+	private String name1;
+	private String name2;
 	
+	public Expense() {
+		this(0, 0.0, "Person 1", "Person 2");
+	}
 	
-	public Expense(int person, double amount){
+	public Expense(int person, double amount, String name1, String name2){
 		this.person = person;
 		this.expenseAmount = amount;
 		this.dateFormat = new SimpleDateFormat("MM/dd/yy");
+		this.name1 = name1;
+		this.name2 = name2;
 	}
 	
 	public void addDescription(String description) {
@@ -42,12 +44,22 @@ public class Expense {
 		String person;
 		String str = "";
 		if (this.person == 0) {
-			person = "Caleb";
+			person = name1;
 		}
-		else person = "Grace";
+		else person = name2;
 		
 		str += person + " | $" + this.expenseAmount + " | " + this.description + " | " + this.paid + " | " + dateFormat.format(date);
 		
 		return str;
+	}
+
+	public String getName2() {
+		// TODO Auto-generated method stub
+		return name2;
+	}
+
+	public String getName1() {
+		// TODO Auto-generated method stub
+		return name1;
 	}
 }
